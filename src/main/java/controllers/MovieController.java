84b901032031ca201073703d90f09380ce3bbd53
Unknown <krishna.arjun96@gmail.com>
2018-03-07 01:41:12 -0500

    Renamed controller

--- src/main/java/controllers/MovieController.java
+++ src/main/java/controllers/MovieController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class HelloController {
+public class MovieController {
 
     @RequestMapping("/")
     public String index() {
