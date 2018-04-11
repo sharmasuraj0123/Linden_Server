ecebd47e26ae9e2bf0190503aee11c869cd09031
Unknown <krishna.arjun96@gmail.com>
2018-04-11 12:45:59 -0400

    migrated script to py3

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import requests
 import json
-import MySQLdb
+import pymysql
 import sys
+
+# CMD
+# sudo apt-get install python3-pymysql
 
 # http://www.omdbapi.com/?i=tt0137523&apikey=12d0adae
 omdb_url = 'http://www.omdbapi.com'
@@ -19,13 +22,13 @@ username = sys.argv[1]
 password = sys.argv[2]
 
 try:
-	db = MySQLdb.connect(user=username, passwd=password, host='127.0.0.1')
+	db = pymysql.connect(user=username, passwd=password, host='127.0.0.1')
 	cur = db.cursor()
-	print 'Connected to MySQL Server.'
+	print('Connected to MySQL Server.')
 	cur.execute('CREATE DATABASE IF NOT EXISTS linden')
 	cur.execute('use linden')
-	print 'Using Linden'
-	cur.execute('DROP TABLE IF EXISTS movies')
+	print('Using Linden')
+	cur.execute('DROP TABLE IF EXISTS Movies')
 	# create movie table
 	cur.execute("""CREATE TABLE IF NOT EXISTS
 			Movies(
@@ -34,39 +37,60 @@ try:
 			Year 	INTEGER NOT NULL,
 			PRIMARY KEY(Id))
 			""")
-	print 'Table Created!'
+	print('Table Created!')
 	#sys.exit()
 
-except MySQLdb.Error as e:
-	print 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
+except pymysql.Error as e:
+	print('MySQL Error [%d]: %s' % (e.args[0], e.args[1]))
 	sys.exit()
 
 
-movie_id = 1
+movie_id = 0
 counter = 0
 
-while(counter < 100):
+while(True):
+	movie_id += 1
 	# Hitting the tmdb API
-	tmdb_response = requests.get(url = tmdb_url + str(movie_id) +'?api_key=' +tmdb_key)
-	tmdb_data = tmdb_response.json()
-
-	if 'status_code' in tmdb_data:
-		movie_id += 1
-		# print(tmdb_response)
+	try:
+		tmdb_response = requests.get(url = tmdb_url + str(movie_id) +'?api_key=' +tmdb_key)
+		tmdb_data = tmdb_response.json()
+		print("Success: 1")
+	except:
+		print(tmdb_response)
 		continue
 
-	# extrating the imdb ID
-	imdb_id = tmdb_data['imdb_id']
-
-	# Hitting the omdb API
-	omdb_response = requests.get(url = omdb_url + '?i=' + imdb_id + '&apikey=' + omdb_key)
-
-	data = omdb_response.json()
+	if 'status_code' in tmdb_data:
+		# movie_id += 1
+		# print(tmdb_response)
+		continue
 	
-	cur.execute("INSERT INTO movies (Id, Name, Year) VALUES(\""+imdb_id+"\",\" "+data['Title']+"\", "+data['Year']+")")
+	try:
+		# extrating the imdb ID
+		imdb_id = tmdb_data['imdb_id']
 
-	db.commit()	
-	print('Number: ' + str(counter + 1))
+		# Hitting the omdb API
+		omdb_response = requests.get(url = omdb_url + '?i=' + imdb_id + '&apikey=' + omdb_key)
+		data = omdb_response.json()
+		print("Success 2")
+	except:
+		print(omdb_response)
+		continue
+	
+	if 'Title' not in data:
+		print('ERROR: NO TITLE FOUND')
+		print(data)
+		sys.exit()
+		continue
+
+	try:
+		cur.execute("INSERT INTO Movies (Id, Name, Year) VALUES(\""+imdb_id+"\",\" "+data['Title']+"\", "+data['Year']+")")
+	except:
+		print(imdb_id+"\",\" "+data['Title']+"\", "+data['Year'])
+		# sys.exit()
+
+	db.commit()
+	
+	print('Number: ' + str(movie_id))
 	if 'Title' in data: 	print('Title: ' + data['Title'])
 	if 'Released' in data: 	print('Released: ' + data['Released'])
 	if 'Year' in data:	print('Year: ' + data['Year'])
@@ -79,8 +103,9 @@ while(counter < 100):
 	if 'Genre' in data:	print('Genre: ' + data['Genre'])
 	if 'Rated' in data:	print('Rating: ' + data['Rated'])
 	print('----------------------------------------------------------')
-	movie_id += 1
+	# movie_id += 1
 	counter += 1
+
 
 cur.close()
 db.close()
