45677da139357bf41a20a1bfcab4fa9bb2dd77df
Unknown <krishna.arjun96@gmail.com>
2018-04-14 19:49:05 -0400

    Wildcard for CORS.

--- src/main/java/com/linden/config/ApplicationConfig.java
+++ src/main/java/com/linden/config/ApplicationConfig.java
@@ -27,8 +27,7 @@ public class ApplicationConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(
-                        "http://localhost:3000", "http://*:*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
