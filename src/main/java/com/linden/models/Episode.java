9b5316d299ada969b781267f937019f026f5b4cd
adityapotdar <aditya.potdar@stonybrook.edu>
2018-05-06 14:14:45 -0400

    added beans for episode and season

--- src/main/java/com/linden/models/Episode.java
+++ src/main/java/com/linden/models/Episode.java
@@ -1,4 +1,71 @@
 package com.linden.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
 public class Episode {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    protected long id;
+
+    protected String name;
+
+    protected int episodeNumber;
+
+    protected String details;
+
+    protected Date releaseDate;
+
+    protected String poster;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
--- src/main/java/com/linden/models/Season.java
+++ src/main/java/com/linden/models/Season.java
@@ -1,4 +1,84 @@
 package com.linden.models;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Season {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    protected long id;
+
+    protected String name;
+
+    protected String details;
+
+    protected int seasonNumber;
+
+    protected int numberOfEpisodes;
+
+    protected Date releaseDate;
+
+    protected String poster;
+
+    @OneToMany
+    protected List<Episode> episodes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public int getNumberOfEpisodes() {
+        return numberOfEpisodes;
+    }
+
+    public void setNumberOfEpisodes(int numberOfEpisodes) {
+        this.numberOfEpisodes = numberOfEpisodes;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
