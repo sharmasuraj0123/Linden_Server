5a114c8412a9e18af5208e58829a0ba95b1d2190
Kenneth <chiguichon007@gmail.com>
2018-05-12 20:45:49 -0400

    fixed ContentContainer?

--- src/main/java/com/linden/util/ContentContainer.java
+++ src/main/java/com/linden/util/ContentContainer.java
@@ -1,12 +1,14 @@
 package com.linden.util;
 
 import com.linden.models.content.Content;
+import com.linden.models.content.Movie;
+import com.linden.models.content.TvShow;
 
 public class ContentContainer {
 
     private String token;
 
-    private Content content;
+    private ContentContainerObject obj;
 
     public ContentContainer() {
 
@@ -14,7 +16,23 @@ public class ContentContainer {
 
     public ContentContainer(String token, Content content) {
         this.token = token;
-        this.content = content;
+        this.obj = new ContentContainerObject();
+        if (content != null) {
+            this.obj.setId(content.getId());
+            this.obj.setBoxOffice(content.getBoxOffice());
+            this.obj.setCast(content.getCast());
+            this.obj.setContentType(content.getContentType());
+            this.obj.setDetails(content.getDetails());
+            this.obj.setGenre(content.getGenre());
+            this.obj.setLindenMeter(content.getLindenMeter());
+            this.obj.setName(content.getName());
+            this.obj.setPhotos(content.getPhotos());
+            this.obj.setPoster(content.getPoster());
+            this.obj.setReleaseDate(content.getReleaseDate());
+            this.obj.setReviews(content.getReviews());
+            this.obj.setScore(content.getScore());
+            this.obj.setVideos(content.getVideos());
+        }
     }
 
     public String getToken() {
@@ -25,11 +43,61 @@ public class ContentContainer {
         this.token = token;
     }
 
+    public ContentContainerObject getObj() {
+        return obj;
+    }
+
+    public void setObj(ContentContainerObject obj) {
+        this.obj = obj;
+    }
+
     public Content getContent() {
+        Content content = null;
+        if(obj != null) {
+            switch (obj.getContentType()){
+                default:
+                case MOVIE:
+                    content = new Movie();
+                    break;
+                case TVSHOW:
+                    content = new TvShow();
+                    break;
+            }
+            content.setId(this.obj.getId());
+            content.setBoxOffice(this.obj.getBoxOffice());
+            content.setCast(this.obj.getCast());
+            content.setContentType(this.obj.getContentType());
+            content.setDetails(this.obj.getDetails());
+            content.setGenre(this.obj.getGenre());
+            content.setLindenMeter(this.obj.getLindenMeter());
+            content.setName(this.obj.getName());
+            content.setPhotos(this.obj.getPhotos());
+            content.setPoster(this.obj.getPoster());
+            content.setReleaseDate(this.obj.getReleaseDate());
+            content.setReviews(this.obj.getReviews());
+            content.setScore(this.obj.getScore());
+            content.setVideos(this.obj.getVideos());
+        }
         return content;
     }
 
     public void setContent(Content content) {
-        this.content = content;
+        this.obj = new ContentContainerObject();
+        if (content != null) {
+            this.obj.setId(content.getId());
+            this.obj.setBoxOffice(content.getBoxOffice());
+            this.obj.setCast(content.getCast());
+            this.obj.setContentType(content.getContentType());
+            this.obj.setDetails(content.getDetails());
+            this.obj.setGenre(content.getGenre());
+            this.obj.setLindenMeter(content.getLindenMeter());
+            this.obj.setName(content.getName());
+            this.obj.setPhotos(content.getPhotos());
+            this.obj.setPoster(content.getPoster());
+            this.obj.setReleaseDate(content.getReleaseDate());
+            this.obj.setReviews(content.getReviews());
+            this.obj.setScore(content.getScore());
+            this.obj.setVideos(content.getVideos());
+        }
     }
 }
---
+++ src/main/java/com/linden/util/ContentContainerObject.java
@@ -0,0 +1,155 @@
+package com.linden.util;
+
+import com.linden.models.content.Cast;
+import com.linden.models.content.ContentType;
+import com.linden.models.content.Genre;
+import com.linden.models.content.Review;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public class ContentContainerObject implements Serializable {
+    
+    private long id;
+    private String name;
+    private String details;
+    private Date releaseDate;
+    private String videos;
+
+    private double score;
+
+    private double lindenMeter;
+
+    private double boxOffice;
+
+    private Set<String> photos;
+
+
+    private Set<Cast> cast;
+
+    private Set<Genre> genre;
+
+
+    private List<Review> reviews;
+    private String poster;
+
+
+    private ContentType contentType;
+
+    public ContentContainerObject() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getVideos() {
+        return videos;
+    }
+
+    public void setVideos(String videos) {
+        this.videos = videos;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public double getLindenMeter() {
+        return lindenMeter;
+    }
+
+    public void setLindenMeter(double lindenMeter) {
+        this.lindenMeter = lindenMeter;
+    }
+
+    public double getBoxOffice() {
+        return boxOffice;
+    }
+
+    public void setBoxOffice(double boxOffice) {
+        this.boxOffice = boxOffice;
+    }
+
+    public Set<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Set<String> photos) {
+        this.photos = photos;
+    }
+
+    public Set<Cast> getCast() {
+        return cast;
+    }
+
+    public void setCast(Set<Cast> cast) {
+        this.cast = cast;
+    }
+
+    public Set<Genre> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Set<Genre> genre) {
+        this.genre = genre;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(ContentType contentType) {
+        this.contentType = contentType;
+    }
+}
